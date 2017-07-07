2380dfd9edc9d376286396ee4ec3d24430313e2d
jonathanhsong <jonathan.h.song@gmail.com>
2017-07-07 10:43:17 -0400

    Update mtaDM.py

--- mtaDM.py
+++ mtaDM.py
@@ -3,13 +3,13 @@ api = twitter.Api(consumer_key='',consumer_secret='',access_token_key='',access_
 
 #Documentation is wrong. Text, receiver,
 def oneTrain_delay():
-	api.PostDirectMessage('1 2 3 Delays', "1559767622")
+	api.PostDirectMessage('1 2 3 Delays', "user_id")
 
 def oneTrain_status():
 	api.PostUpdate('1 2 3 Subway Status no longer on time')
 
 def aceTrain_delay():
-	api.PostDirectMessage('A C E Delays', "1559767622")	
+	api.PostDirectMessage('A C E Delays', "user_id")	
 
 def aceTrain_status():
 	api.PostUpdate('A C E Subway Status no longer on time')
